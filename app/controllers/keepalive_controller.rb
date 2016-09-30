f999865c34daec276b01e87b05a92fc936bc053a
Mario Andrés Correa <a@macool.me>
2016-09-30 08:48:39 -0500

    mr thomas is not an asset anymore

--- app/controllers/keepalive_controller.rb
+++ app/controllers/keepalive_controller.rb
@@ -1,6 +1,6 @@
 class KeepaliveController < ApplicationController
   def show
-    filename = ActionController::Base.helpers.asset_path "mr-thomas.png"
+    filename = Rails.root.join "public", "images", "mr-thomas.png"
     send_file filename, disposition: "inline"
   end
 end
