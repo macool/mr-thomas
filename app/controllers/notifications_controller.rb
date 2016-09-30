ba606f5fb80e8c6892607201eb363efe2cbf5305
Mario Andrés Correa <a@macool.me>
2016-09-29 20:36:41 -0500

    create notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,10 +4,22 @@ class NotificationsController < ApplicationController
 
   def create
     puts params
+    @subscriber.notifications.create!(
+      referrer: request.referrer,
+      parameters: permitted_params
+    )
     head :ok
   end
 
   private
+
+  def permitted_params
+    params.permit(
+      :name,
+      :email,
+      :message
+    )
+  end
 
   def find_subscriber
     @subscriber = Subscriber.find_by(
