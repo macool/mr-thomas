a26f87db59b36eeec86d0a23e2a861b350a2afa5
Mario Andrés Correa <a@macool.me>
2016-09-30 07:38:23 -0500

    seed fedes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,23 @@
     host: "localhost",
     name: "fedes-local",
     recipient: "a@macool.me"
+  },
+  {
+    host: "fedes-dev.herokuapp.com",
+    name: "fedes-dev",
+    recipient: "a@macool.me"
+  },
+  {
+    host: "fedes.ec",
+    name: "fedes-production",
+    recipient: "a@macool.me"
   }
 ].each do |attrs|
-  subscriber = Subscriber.create(attrs)
-  puts "creating subscriber: #{subscriber.pretty_print}"
+  if Subscriber.where(name: attrs[:name]).exists?
+    subscriber = Subscriber.find_by name: attrs[:name]
+    puts "#{attrs[:name]} already exists! #{subscriber.pretty_print}"
+  else
+    subscriber = Subscriber.create(attrs)
+    puts "creating subscriber: #{subscriber.pretty_print}"
+  end
 end
