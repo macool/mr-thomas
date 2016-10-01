b39f0ac68911e59daf674197a4198c87861e8d42
Mario Andrés Correa <a@macool.me>
2016-10-01 08:03:23 -0500

    try not to log twice same client

--- app/controllers/keepalive_controller.rb
+++ app/controllers/keepalive_controller.rb
@@ -1,5 +1,5 @@
 class KeepaliveController < ApplicationController
-  before_action :log_keepalive!
+  before_action :process_keepalive!
 
   def show
     filename = Rails.root.join "public", "images", "mr-thomas.png"
@@ -8,11 +8,36 @@ class KeepaliveController < ApplicationController
 
   private
 
-  def log_keepalive!
-    keep_alive = KeepAlive.new(
+  def process_keepalive!
+    @keep_alive = KeepAlive.new(
       referrer: request.referrer,
       request_ip: request.remote_ip
     )
-    keep_alive.save unless keep_alive.localhost?
+    persist_keepalive_if_needed!
+    set_client!
+  end
+
+  def persist_keepalive_if_needed!
+    if !@keep_alive.localhost?
+      @keep_alive.save if new_client?
+    end
+  end
+
+  ##
+  # be sure to call #new_client? ONLY
+  # before #set_client!
+  def new_client?
+    host_in_session = session.fetch(
+      "host_#{@keep_alive.host}", nil
+    )
+    host_in_session.blank? || (
+      Time.parse(host_in_session) < 1.hour.ago
+    )
+  end
+
+  def set_client!
+    session[
+      "host_#{@keep_alive.host}"
+    ] = Time.now.to_s
   end
 end
